721ac69af04a94d3a5cc44bdc052bd000af73a6e
selidrissi <s.elidrissi1992@gmail.com>
2026-01-02 20:56:12 +0100

    init project

---
+++ src/main/java/com/sei/users/UsersReactiveSpringApplication.java
@@ -0,0 +1,13 @@
+package com.sei.users;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsersReactiveSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UsersReactiveSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sei/users/UsersReactiveSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sei.users;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsersReactiveSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
